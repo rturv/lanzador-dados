367e00d229353bdc80c094b7d554f76c30077c0a
rtur <ramon.tur@soltel.es>
2026-01-31 01:30:49 +0100

    Corregir base path para repositorio lanzador-dados

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dados/', // Cambia 'dados' por el nombre exacto de tu repositorio
+  base: '/lanzador-dados/', // Nombre correcto del repositorio
   build: {
     rollupOptions: {
       input: {
